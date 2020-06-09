e5baa956ddb6d3cee8bc2584290f5b5a91090d7f
Artem Lipovatyi <artemlipovatyi@gmail.com>
2020-06-09 13:54:52 +0200

    Add dummy joke remote repository

---
+++ lib/data/remote/repository/joke_remote_repository.dart
@@ -0,0 +1,9 @@
+import 'package:flutternetworking/data/remote/dto/joke_dto.dart';
+
+class JokeRemoteRepository {
+
+  Future<JokeDto> getRandomJoke() async {
+    // TODO: replace with valid implementation
+    return JokeDto('', '', '');
+  }
+}
