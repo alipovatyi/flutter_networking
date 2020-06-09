3765072e7bd3ec055a63e0ea2b3f1cf0f5ac3015
Artem Lipovatyi <artemlipovatyi@gmail.com>
2020-06-09 19:18:34 +0200

    Add joke remote repository implementation

--- lib/data/remote/repository/joke_remote_repository.dart
+++ lib/data/remote/repository/joke_remote_repository.dart
@@ -1,11 +1,16 @@
+import 'dart:convert';
+
+import 'package:flutternetworking/data/remote/api_client.dart';
 import 'package:flutternetworking/data/remote/dto/joke_dto.dart';
 
 class JokeRemoteRepository {
+  JokeRemoteRepository(this._apiClient) : assert(_apiClient != null);
+
+  final ApiClient _apiClient;
 
   Future<JokeDto> getRandomJoke() async {
-    // TODO: replace with valid implementation
-    final duration = const Duration(seconds: 1);
-    final jokeDto = const JokeDto('id', 'Test joke', null);
-    return Future.delayed(duration, () => jokeDto);
+    final response = await _apiClient.get('/jokes/random');
+    final json = jsonDecode(response.body);
+    return JokeDto.fromJson(json);
   }
 }
--- test/data/remote/repository/joke_remote_repository_test.dart
+++ test/data/remote/repository/joke_remote_repository_test.dart
@@ -1,1 +1,35 @@
-void main() {}
+import 'package:flutter_test/flutter_test.dart';
+import 'package:flutternetworking/data/remote/api_client.dart';
+import 'package:flutternetworking/data/remote/repository/joke_remote_repository.dart';
+import 'package:mockito/mockito.dart';
+
+import '../../../fake_data/dto/joke_dto_fake_data.dart';
+import '../util/response_utils.dart';
+
+class MockApiClient extends Mock implements ApiClient {}
+
+void main() {
+  group('assertion', () {
+    test('does not assert if not null', () {
+      expect(() => JokeRemoteRepository(MockApiClient()), returnsNormally);
+    });
+    test('asserts if null', () {
+      expect(() => JokeRemoteRepository(null), throwsAssertionError);
+    });
+  });
+  group('getRandomJoke', () {
+    final apiClient = MockApiClient();
+    final repository = JokeRemoteRepository(apiClient);
+    test('returns JokeDto if response is valid', () async {
+      final responseFileName = 'joke/response/random_joke.json';
+      final response = createResponse(fileName: responseFileName);
+      when(apiClient.get('/jokes/random')).thenAnswer((_) async => response);
+      expect(await repository.getRandomJoke(), equals(createJokeDto()));
+    });
+    test('returns JokeDto if response is valid', () async {
+      final response = createResponse(body: '{}');
+      when(apiClient.get('/jokes/random')).thenAnswer((_) async => response);
+      expect(() => repository.getRandomJoke(), throwsException);
+    });
+  });
+}
