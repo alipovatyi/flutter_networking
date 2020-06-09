9a70be7c68c487640571b0b9a96d67bd7eba29b6
Artem Lipovatyi <artemlipovatyi@gmail.com>
2020-06-09 11:09:59 +0200

    Make joke model equatable

--- lib/data/model/joke.dart
+++ lib/data/model/joke.dart
@@ -1,6 +1,11 @@
-class Joke {
+import 'package:equatable/equatable.dart';
+
+class Joke extends Equatable {
   Joke(this.value, this.iconUrl) : assert(value != null);
 
   final String value;
   final String iconUrl;
+
+  @override
+  List<Object> get props => [value, iconUrl];
 }
