5ccf3b3b1743f29c0f2a19dfffaf584225cce3eb
Artem Lipovatyi <artemlipovatyi@gmail.com>
2020-06-09 11:23:53 +0200

    Add joke dto

---
+++ lib/data/remote/dto/joke_dto.dart
@@ -0,0 +1,23 @@
+import 'package:equatable/equatable.dart';
+import 'package:json_annotation/json_annotation.dart';
+
+part 'joke_dto.g.dart';
+
+@JsonSerializable()
+class JokeDto extends Equatable {
+  JokeDto(this.id, this.value, this.iconUrl);
+
+  final String id;
+  @JsonKey(required: true)
+  final String value;
+  @JsonKey(name: 'icon_url')
+  final String iconUrl;
+
+  factory JokeDto.fromJson(Map<String, dynamic> json) =>
+      _$JokeDtoFromJson(json);
+
+  Map<String, dynamic> toJson() => _$JokeDtoToJson(this);
+
+  @override
+  List<Object> get props => [id, value, iconUrl];
+}
