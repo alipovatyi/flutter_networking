ff1c3da9170f84c307fb2f6396028fd073b1866d
Artem Lipovatyi <artemlipovatyi@gmail.com>
2020-06-10 17:04:07 +0200

    Fetch random choke using Chopper library

--- lib/app.dart
+++ lib/app.dart
@@ -1,9 +1,18 @@
+import 'package:chopper/chopper.dart';
 import 'package:flutter/material.dart';
 import 'package:flutternetworking/data/remote/repository/joke_remote_repository.dart';
+import 'package:flutternetworking/data/remote/service/joke_service.dart';
 import 'package:flutternetworking/data/repository/joke_repository.dart';
 import 'package:flutternetworking/feature/home/home_page.dart';
 
 class App extends StatelessWidget {
+  final chopperClient = ChopperClient(
+    baseUrl: 'https://api.chucknorris.io',
+    services: [
+      JokeService.create(),
+    ],
+  );
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -14,7 +23,9 @@ class App extends StatelessWidget {
       ),
       home: HomePage(
         title: 'Chuck Norris',
-        jokeRepository: JokeRepository(JokeRemoteRepository()),
+        jokeRepository: JokeRepository(
+          JokeRemoteRepository(chopperClient.getService<JokeService>()),
+        ),
       ),
     );
   }
