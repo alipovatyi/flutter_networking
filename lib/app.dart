e788aaf05e1b4a594e11e924de2c1b374c62a3b6
Artem Lipovatyi <artemlipovatyi@gmail.com>
2020-06-10 21:07:05 +0200

    Fetch random joke using Retrofit library

--- lib/app.dart
+++ lib/app.dart
@@ -1,11 +1,16 @@
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:flutternetworking/data/remote/repository/joke_remote_repository.dart';
+import 'package:flutternetworking/data/remote/service/joke_service.dart';
 import 'package:flutternetworking/data/repository/joke_repository.dart';
 import 'package:flutternetworking/feature/home/home_page.dart';
 
 class App extends StatelessWidget {
+  final _dio = Dio();
+
   @override
   Widget build(BuildContext context) {
+    _dio.interceptors.add(LogInterceptor(responseBody: false));
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
@@ -14,7 +19,14 @@ class App extends StatelessWidget {
       ),
       home: HomePage(
         title: 'Chuck Norris',
-        jokeRepository: JokeRepository(JokeRemoteRepository()),
+        jokeRepository: JokeRepository(
+          JokeRemoteRepository(
+            JokeService(
+              _dio,
+              baseUrl: 'https://api.chucknorris.io/',
+            ),
+          ),
+        ),
       ),
     );
   }
